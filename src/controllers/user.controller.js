61a099a9dacbaf750d70caf470b1df38bb804d56
Husni Mubarok <husnirezky2@gmail.com>
2025-12-18 17:10:15 +0700

    menit 1 jam 13

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,3 +34,52 @@ export const createUser = async (req, res) => {
     });
   }
 };
+
+export const updateUser = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {id: Number(req.params.id)},
+  });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+      result: null,
+    })
+  }
+
+  //data validation
+  const { error, value } = userValidation(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details[0].message,
+      result: null,
+    })
+  }
+  let pass = user.password;
+  if (value.password && value.password.length > 0){
+    pass = encrypt(value.password);
+  }
+  try{
+    const result = await prisma.user.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: value.name,
+        userName: value.userName,
+        password: pass,
+        role: value.role,
+      },
+    });
+    result.password = "xxxxxxxxxxxxxx";
+    return res.status(200).json({
+      message: "success",
+      result,
+    });
+  } catch (error) {
+    logger.error("controllers/user.controller.js:updateUser: " + error.message);
+    return res.status(500).json({
+      message: error.message,
+      result: null,
+    });
+  }
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import {createUser, updateUser} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.post("/users", createUser);
+userRouter.put("/users/:id", updateUser);
 
 export default userRouter;
