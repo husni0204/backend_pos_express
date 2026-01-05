a1c13f1e8e3b9c126aff9a5db964156be92dc72e
Husni Mubarok <husnirezky2@gmail.com>
2026-01-05 15:54:14 +0700

    menit 2:46:59

--- src/controllers/supplier.controller.js
+++ src/controllers/supplier.controller.js
@@ -1,6 +1,9 @@
 import prisma from "../utils/client.js";
 import { logger } from "../utils/winston.js";
 import { supplierValidation } from "../validations/supplier.validation.js";
+import fs from "fs";
+import pdf from "pdf-creator-node";
+import excelJS from "exceljs";
 
 export const getAllSupplier = async (req, res) => {
     const last_id = parseInt(req.query.lastId) || 0;
@@ -145,6 +148,115 @@ export const deleteSupplier = async (req, res) => {
     } catch (error) {
         logger.error(
             "controller/supplier.controller.js:deleteSupplier - " + error.message
+        );
+        return res.status(500).json({ message: error.message, result: null });
+    }
+};
+
+export const generatePdf = async (req, res) => {
+    let pathFile = "./public/pdf";
+    let fileName = "supplier.pdf";
+    let fullPath = pathFile + "/" + fileName;
+    let html = fs.readFileSync("./src/templates/supplierTemplate.html", "utf8");
+    let options = {
+        format: "A4",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "0.1mm",
+            contents: "",
+        },
+        footer: {
+            height: "28mm",
+            contents: {
+                default: 
+                    '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            },
+        },
+    };
+    try {
+        if (fs.existsSync(fullPath)) {
+            fs.unlinkSync(fullPath);
+        }
+        const data = await prisma.supplier.findMany({});
+        let suppliers = [];
+        data.forEach((supplier, no) => {
+            suppliers.push({
+                no: no + 1,
+                name: supplier.firstName + " " + (supplier.lastName ? supplier.lastName : ""),
+                phone: supplier.phone,
+                email: supplier.email,
+                address: supplier.address,
+            });
+        });
+        let document = {
+            html: html,
+            data: {
+                suppliers: suppliers,
+            },
+            path: fullPath,
+            type: "",
+        };
+        const process = await pdf.create(document, options);
+        if (process) {
+            return res.status(200).json({
+                message: "Berhasil membuat PDF supplier",
+                result: "/pdf/" + fileName,
+            });
+        }
+    } catch (error) {
+        logger.error(
+            "controller/supplier.controller.js:generatePdf - " + error.message
+        );
+        return res.status(500).json({ message: error.message, result: null });
+    }
+};
+
+export const generateExcel = async (req, res) => {
+    const workbook = new excelJS.Workbook();
+    const worksheet = workbook.addWorksheet("Supplier");
+    const path = "./public/excel";
+    try {
+        if (fs.existsSync(`${path}/Supplier.xlsx`)) {
+            fs.unlinkSync(`${path}/Supplier.xlsx`);
+        }
+        const data = await prisma.supplier.findMany({});
+        worksheet.columns = [
+            { header: "No", key: "no", width: 5 },
+            { header: "Name", key: "name", width: 25 },
+            { header: "Phone", key: "phone", width: 15 },
+            { header: "Email", key: "email", width: 35 },
+            { header: "Address", key: "address", width: 50 },
+        ];
+        let counter = 1;
+        data.map((supplier) => {
+            supplier.no = counter;
+            supplier.name = supplier.firstName + " " + (supplier.lastName ? supplier.lastName : "");
+            worksheet.addRow(supplier);
+            counter++;
+        });
+        let list = ["A", "B", "C", "D", "E"];
+        for (let i = 0; i <= counter; i++) {
+            list.forEach((item) => {
+                worksheet.getCell(item + i).border = {
+                    top: { style: "thin" },
+                    left: { style: "thin" },
+                    bottom: { style: "thin" },
+                    right: { style: "thin"},
+                };
+            });
+        };
+        worksheet.getRow(1).eachCell((cell) => {
+            cell.font = { bold: true };
+        });
+        await workbook.xlsx.writeFile(`${path}/Supplier.xlsx`);
+        return res.status(200).json({
+            message: "Berhasil membuat Excel Supplier",
+            result: "/excel/Supplier.xlsx",
+        });
+    } catch (error) {
+        logger.error(
+            "controller/supplier.controller.js:generateExcel - " + error.message
         );
         return res.status(500).json({ message: error.message, result: null });
     }
--- src/routes/supplier.router.js
+++ src/routes/supplier.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { autenticate } from "../controllers/error.controller.js";
-import { createSupplier, deleteSupplier, getAllSupplier, getSupplierById, updateSupplier } from "../controllers/supplier.controller.js";
+import { createSupplier, deleteSupplier, generateExcel, generatePdf, getAllSupplier, getSupplierById, updateSupplier } from "../controllers/supplier.controller.js";
 
 
 const supplierRouter = Router();
@@ -10,5 +10,7 @@ supplierRouter.get("/suppliers/:id", autenticate, getSupplierById);
 supplierRouter.post("/suppliers", autenticate, createSupplier);
 supplierRouter.put("/suppliers/:id", autenticate, updateSupplier);
 supplierRouter.delete("/suppliers/:id", autenticate, deleteSupplier);
+supplierRouter.get("/suppliers-pdf", autenticate, generatePdf);
+supplierRouter.get("/suppliers-excel", autenticate, generateExcel);
 
 export default supplierRouter;
